fe3fa908c385a31df1b5b72363fe9cb6bfe575dd
Nick Morhun <nickmorgunn@gmail.com>
2019-12-17 21:50:05 +0200

    Added the project files

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+	private void Start()
+	{
+		var testGameObject = new GameObject("test");
+		var component = testGameObject.AddComponent<MyComponent>();
+		DestroyImmediate(testGameObject);
+
+		try
+		{
+			component.CallTest();
+			Debug.Log($"{nameof(MyComponent.CallTest)}: No exception was thrown.");
+		}
+		catch (MissingReferenceException ex)
+		{
+			Debug.LogException(ex);
+		}
+
+		try
+		{
+			component.CallTestWithCheck();
+			Debug.Log($"{nameof(MyComponent.CallTestWithCheck)}: No exception was thrown.");
+		}
+		catch (MissingReferenceException ex)
+		{
+			Debug.LogException(ex);
+		}
+	}
+}
---
+++ Assets/Scripts/MyComponent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MyComponent : MonoBehaviour
+{
+    internal void CallTestWithCheck()
+    {
+        if (this)
+        {
+            name += "";
+        }
+        else
+        {
+            Debug.Log("'this' is false!");
+        }
+    }
+
+    internal void CallTest()
+    {
+        name += "";
+    }
+}
